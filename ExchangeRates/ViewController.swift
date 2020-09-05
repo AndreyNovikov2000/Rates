3269f1df5b2e3583ea20088ff59c2f26743dbe24
Andrey Novikov <andrenovikov2000@mail.ru>
2020-09-05 16:40:30 +0600

    Initial Commit

---
+++ ExchangeRates/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExchangeRates
+//
+//  Created by Andrey Novikov on 9/5/20.
+//  Copyright © 2020 Andrey Novikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
